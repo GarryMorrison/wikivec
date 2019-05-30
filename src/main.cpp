0f29e8dbf95d9abaac01c7011307cb3357eec9ae
Garry Morrison <garry@semantic-db.org>
2019-05-30 13:47:34 +0930

    experimenting with tweaks
    trying to find faster code, but vectors don't seem consistently faster
    than original set version.

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <map>
 #include <set>
+#include <unordered_set>
 #include <vector>
 #include <iostream>
 #include <fstream>
@@ -14,8 +15,8 @@ const std::string op = "wikivec";
 // const std::string source = "sw/small.sw";
 const std::string source = "sw/30k--wikivec.sw";
 // const std::string source = "sw/300k--wikivec.sw";
-const bool interactive = true;
-// const bool interactive = false;
+// const bool interactive = true;
+const bool interactive = false;
 
 
 typedef unsigned long ulong;
@@ -75,6 +76,48 @@ std::map<std::string, std::set<ulong> > load_sw(const std::string filename, cons
     infile.close();
     return result;
 }
+
+std::map<std::string, std::vector<ulong> > load_sw_vec(const std::string filename, const std::string op) {
+    std::map<std::string, std::vector<ulong> > result;
+    std::string op_head = op + " |";
+    size_t op_head_length = op_head.length();
+
+    std::string line;
+    std::ifstream infile;
+    infile.open(filename);
+    if (!infile.is_open()) {
+        std::cerr << "Failed to load file: " << filename << std::endl;
+        std::_Exit(0);
+    }
+    while(std::getline(infile, line)) {
+        if (line.rfind(op_head, 0) == 0) {
+            size_t break_pos = line.find("> => ");
+            std::string label = line.substr(op_head_length, break_pos - op_head_length);
+            std::string tail = line.substr(break_pos + 6, line.size() - break_pos - 7);
+
+            std::vector<ulong> vec_tail;
+            size_t pos_start = 0, pos_end, delim_len = 5;
+            while ((pos_end = tail.find("> + |", pos_start)) != std::string::npos) {
+                std::string token = tail.substr(pos_start, pos_end - pos_start);
+                pos_start = pos_end + delim_len;
+                ulong value = std::stoul(token, nullptr, 16);
+                vec_tail.push_back(value);
+            }
+            std::string token = tail.substr(pos_start);
+            ulong value = std::stoul(token, nullptr, 16);
+            vec_tail.push_back(value);
+
+            result.insert(std::make_pair(label, vec_tail));
+
+            // std::cout << "line: " << line << std::endl;
+            // std::cout << "label: " << label << std::endl;
+            // std::cout << "tail: " << tail << std::endl;
+        }
+    }
+    infile.close();
+    return result;
+}
+
 
 // assumes a well constructed sw file!
 std::map<std::string, std::set<ulong> > first_load_sw(const std::string filename, const std::string op) {
@@ -215,6 +258,128 @@ std::vector<std::pair<float, std::string> > print_wikivec_similarity(const std::
     return result;
 }
 
+
+// vector version, hopefully faster than the set version:
+// code for intersection from here:
+// https://leetcode.com/problems/intersection-of-two-arrays/discuss/266901/C%2B%2B-beats-100.
+ulong first_find_intersection_size(const std::vector<ulong> &A, const std::vector<ulong> &B) {
+        std::vector<ulong> result;
+        std::vector<ulong> one = A, two = B;
+        std::sort(one.begin(), one.end()); 
+        std::sort(two.begin(), two.end());
+        int l = 0, r = 0;
+        while(l < one.size() && r < one.size()){
+            ulong left = one[l], right = two[r];
+            if(left == right){
+                result.push_back(right);
+                while(l < one.size() && one[l] == left )l++;
+                while(r < two.size() && two[r] == right )r++;
+                continue;
+            }
+            if(left < right){
+            while(l < one.size() && one[l] == left )l++;
+            }else while( r < two.size()  && two[r] == right )r++;    
+        }
+        return result.size();
+}
+
+ulong second_find_intersection_size(const std::vector<ulong> &one, const std::vector<ulong> &two) {
+    ulong result = 0;
+    if (one.empty() || two.empty()) {
+        return 0;
+    }
+    std::unordered_set<ulong> set{one.cbegin(), one.cend()};
+    for (auto n: two) {
+        if (set.erase(n) > 0) {
+            result++;
+        }
+    }
+    return result;
+}
+
+ulong find_intersection_size(const std::vector<ulong> &one, const std::vector<ulong> &two) {
+    // std::unordered_set<ulong> us(std::begin(one), std::end(one));
+    std::set<ulong> us(std::begin(one), std::end(one));
+    ulong result = std::count_if(std::begin(two), std::end(two), [&](ulong n) {return us.find(n) != std::end(us);});
+    return result;
+}
+
+ulong find_union_size(const std::vector<ulong> &one, const std::vector<ulong> &two) {
+    // std::unordered_set<ulong> us(std::begin(one), std::end(one));
+    std::set<ulong> us(std::begin(one), std::end(one));
+    for (auto const n: two) {
+        us.insert(n);
+    }
+    return us.size();
+}
+
+ulong second_find_union_size(const std::vector<ulong> &one, const std::vector<ulong> &two) {
+    std::set<ulong> result;
+    set_union(one.begin(), one.end(), two.begin(), two.end(), std::inserter(result, result.begin()));
+    return result.size();
+}
+
+
+float simm(const std::vector<ulong> &one, const std::vector<ulong> &two) {
+    ulong union_size = find_union_size(one, two);
+    ulong intersection_size = find_intersection_size(one, two);
+    if (union_size == 0) { return 0.0; }
+    return (float)intersection_size / (float)union_size;
+}
+
+std::vector<std::pair<float, std::string> > pattern_recognition(const std::map<std::string, std::vector<ulong> > &sw_map, const std::vector<ulong> &pattern, ulong number_of_results) {
+    std::vector<std::pair<float, std::string> > result;
+    for (auto const & pair: sw_map) {
+        float value = simm(pattern, pair.second);
+        if (value > 0) {
+            result.push_back(std::make_pair(value, pair.first));
+        }
+    }
+    std::sort(result.rbegin(), result.rend());
+    result.resize(number_of_results);
+    return result;
+}
+
+std::vector<std::pair<float, std::string> > print_wikivec_similarity(const std::map<std::string, std::vector<ulong> > &sw_map, const std::string &wikipage, int number_of_results) {
+    // test wikipage is in sw_map:
+    if (sw_map.find(wikipage) == sw_map.end()) {
+        std::cout << wikipage << " not in dictionary" << std::endl;
+        std::vector<std::pair<float, std::string> > empty;
+        return empty;
+    }
+
+    // print details header:
+    std::cout << "----------------" << std::endl;
+    std::cout << "vector version" << std::endl;
+    auto pattern = sw_map.at(wikipage);
+    std::cout << "wikipage: " << wikipage << std::endl;
+    std::cout << "pattern: {";
+    for (const auto ket: pattern) {
+        std::cout << ket << ", ";
+    }
+    std::cout << "}" << std::endl;
+    std::cout << "pattern length: " << pattern.size() << std::endl;
+    std::cout << "----------------" << std::endl;
+
+    // find matching patterns:
+    auto result = pattern_recognition(sw_map, pattern, number_of_results);
+
+    // print out a table of results:
+    // std::cout << "result size: " << result.size() << std::endl;
+    int max_width = 0;
+    for (const auto x: result) {
+        max_width = std::max(max_width, (int)x.second.size());
+    }
+    int k = 1;
+    for (const auto x: result) {
+        std::cout << std::left << std::setw(4) << k << std::left << std::setw(max_width + 1) << x.second << " " << (int)(10000.0 * x.first)/100.0 << std::endl;
+        k++;
+    }
+    std::cout << std::endl;
+    return result;
+}
+
+
 int main(int argc, char* argv[]) {
     // first process our command line arguments:
     if (argc < 2 || argc > 3) {
@@ -236,10 +401,25 @@ int main(int argc, char* argv[]) {
 
     // load the sw file:
     auto sw_map = load_sw(source, op);
+    // std::map<std::string, std::set<ulong> > sw_map;    
+    // auto sw_map = load_sw_vec(source, op);
 
     // test it loads:
     // print_sw_map(sw_map);
     // return 0;
+
+    // test union_size and intersection_size:
+    std::vector<ulong> alpha, beta;
+    alpha.push_back(5);
+    alpha.push_back(10);
+    alpha.push_back(15);
+    alpha.push_back(20);
+    beta.push_back(15);
+    beta.push_back(5);
+    beta.push_back(25);
+    std::cout << "alpha intn beta: " << find_intersection_size(alpha, beta) << std::endl;
+    std::cout << "alpha union beta: " << find_union_size(alpha, beta) << std::endl;
+    // return 0;    
 
     // find and print out wikivec similarity:
     auto result = print_wikivec_similarity(sw_map, wikipage, number_of_results);
